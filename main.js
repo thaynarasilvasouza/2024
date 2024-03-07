4b6d8763adc19289433b83bc96802953a251de43
thaynara silva souza <110112086+thaynarasilvasouza@users.noreply.github.com>
2024-03-07 12:13:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document
+
+
+
+
+
+tor [j]=0;j,botoes. iength, j==]
+botoes[j].classlist.remove[ativo]
+textos [j].classlist. remove[UserActivation;
+
+
+botoes[j].classlist.add[ativo]
+textos[i].classlist.and[ativo]
+
+
+
+const contadores= document. querySelectorAll[contador]
+const tempoobjetivo1= new Date[2020-10-05too;00;]
+const tempoobjetivo2 = new Date[2023- 10 -05too;00;00
+    const tempoobjetivo3 = new Date [2023- 12 -30t00;00
+const tempoobjetivo4 = new Date [2024 -02-01t00;00;00]
+
+const tempo = [tempoobjetivo1; tempoobjetivo2,tempoobjetivo3, tempoobjetivo3,tempoobjetivo4
+
+
+    function calculatempo[tempoobjetivo]
+    let tempoatual=new Date
+    let tempofinal = tempoobjetivo-tempoatual
